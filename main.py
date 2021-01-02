822d3cb3cc700daef261ddc00c64de011b34f069
chrizwez <75858170+chrizwez@users.noreply.github.com>
2021-01-02 18:18:48 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-import returnz
+import a_python_quest01
+
+
+#import madlibs
+
+#import number_guess
+
+
+class Horus:
+  """#This is how we do it"""
+  def __init__(self, name, time):
+    """#This constuctor is used to reduce the time it takes to create new variables after giving it this class"""
+    self.name = name
+    self.time = time
+    print("I am {} and my phone is set to {}".format(self.name,self.time))
+
+john = Horus("John", "10am")
+help(Horus)
