2d26dc4cee672861cc22c0dd85133af662434534
chrizwez <chrizwez@gmail.com>
2021-01-04 11:47:48 +0000

    added to-do

--- web_scrap.py
+++ web_scrap.py
@@ -6,22 +6,43 @@ url = "https://en.wikipedia.org/wiki/Inca_Empire"
 response = requests.get(url)
 
 print(response.status_code)
+#print(response.content())
 
 #q = input("you ok? ")
 
 soup = BeautifulSoup(response.content, "html.parser")
 
-#results = soup.find(id="ResultsContainer")
+header = soup.find(id="firstHeading")
+
+body = soup.find_all(id='bodyContent')
+
+bodies = soup.find_all('h3',class_='mw-headline')
+sub = []
+#for bod in bodies:
+#  Subs = bod.find('h3', #class_='mw-headline')
+#  Subs = Subs#.text
+#  sub.append(Subs)
+
+print(bodies)
+#a = input("ok")
+print(header.text)
 
 #print(results.prettify())
 #job_elems = results.find_all('section', class_='card-content')
 #print(job_elems)
 
-class01 = soup.find_all('h1', class_='firstHeading')
+#page = results.find_all()
+#class01 = soup.find_all('center')
 
-print(class01)
-for klass in class01:
-  print(klass)
+#class02 = soup.find('h2')
+
+#print(class02)
+
+
+#print(class01)
+#x = input("is this good? ")
+#for klass in class01:
+ # print(klass)
 
 
 #title = soup.find(id = "firstHeading")
